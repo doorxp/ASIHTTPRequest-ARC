984db647a8f1e45a70e325c07c6c7f4ded230e06
doorxp <doorxp@msn.com>
2015-06-11 10:35:26 +0800

    werer

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -3915,7 +3915,7 @@ static NSOperationQueue *sharedQueue = nil;
 		// Work around <rdar://problem/5530166>.  This dummy call to 
 		// CFNetworkCopyProxiesForURL initialise some state within CFNetwork 
 		// that is required by CFNetworkCopyProxiesForAutoConfigurationScript.
-		CFRelease(CFNetworkCopyProxiesForURL((__bridge CFURLRef)[self url], NULL));
+//		CFRelease(CFNetworkCopyProxiesForURL((__bridge CFURLRef)[self url], NULL));
 
 		// Obtain the list of proxies by running the autoconfiguration script
 		CFErrorRef err = NULL;
